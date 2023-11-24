ffd799315f5e87f0181c0f8a59a17315e041b4d8
EdoPan <81682326+EdoPan@users.noreply.github.com>
2023-11-24 11:58:31 +0100

    Add generateLearningSets.py

---
+++ generateLearningSets/generateLearningSets.py
@@ -0,0 +1,33 @@
+class GenerateLearningSets:
+    def __init__(self):
+        # Builder
+        pass
+    
+    def receiveSession():
+        return
+    
+    def storePreparedSession():
+        # Store the session
+        # Check if there are enough sessions
+        return
+    
+    def generateBalancingReport():
+        return
+    
+    def checkDataBalancing():
+        # Handle the condition of not balanced data
+        return
+    
+    def generateCoverageReport():
+        # Handle the condition of not satisfied coverage
+        return
+    
+    def checkDataQuality():
+        # Handle the condition of poor quality
+        return
+    
+    def generateLearningSets():
+        return
+    
+    def sendLearningSets():
+        return
