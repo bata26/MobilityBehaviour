8e3433caa337ca60a5fba11c935dcc8994d817e3
EdoPan <81682326+EdoPan@users.noreply.github.com>
2024-01-03 15:52:17 +0100

    Fix unit test

--- segregation_system/test/test_balancing_report_generator.py
+++ segregation_system/test/test_balancing_report_generator.py
@@ -1,25 +1,8 @@
+import os
 import sys
 import pytest
 sys.path.insert(0, r'../../segregation_system')
-from src.learning_sets_generator import LearningSetsGenerator
-
-
-@pytest.fixture
-def test_config():
-    return {
-    "stage": "store",
-    "segregation_system_ip": "0.0.0.0",
-    "segregation_system_port": "6000",
-    "development_system_ip": "10.8.0.2",
-    "development_system_port": "6000",
-    "preparation_system_ip": "10.8.0.6",
-    "preparation_system_port": "5000",
-    "db_name": "segregation.db",
-    "max_sessions": 400,
-    "train_set_size": 0.7,
-    "validation_set_size": 0.2,
-    "test_set_size": 0.1
-}
+from src.balancing_report_generator import BalancingReportGenerator
 
 @pytest.fixture
 def test_dataset():
@@ -176,26 +159,22 @@ def test_dataset():
         },
     ]
 
+def test_report(test_dataset):
+    generator = BalancingReportGenerator()
+    info = generator.generate_chart(test_dataset)
 
-def test_generate_learning_sets(test_config, test_dataset):
-    generator = LearningSetsGenerator(test_config)
-    result = generator.generate_learning_sets(test_dataset)
-    assert result["train"]["number_of_samples"] == 7
-    assert result["validation"]["number_of_samples"] == 2
-    assert result["test"]["number_of_samples"] == 1
+    assert os.path.isfile("./data/balancing/balancing_chart.png") == True
 
-    assert len(result["train"]["features"]) == 7
-    assert len(result["validation"]["features"]) == 2
-    assert len(result["test"]["features"]) == 1
+    assert info['shopping_items_number'] == 10
+    assert info['sport_items_number']  == 0
+    assert info['cooking_items_number'] == 0
+    assert info['gaming_items_number'] == 0
+    assert info['shopping_average'] == 1.0
+    assert info['sport_average'] == 0
+    assert info['cooking_average'] == 0
+    assert info['gaming_average'] == 0
+    assert info['threshold'] == 50
 
-    categories = ["train" , "validation" , "test"]
-
-    for category in categories:
-        for feature in result[category]["features"]:
-            assert feature["maximum_pressure_ts"] == 0.0
-            assert feature["minimum_pressure_ts"] == 0.0
-            assert feature["median_pressure_ts"] == 0.0
-            assert feature["mean_absolute_deviation_pressure_ts"] == 0.0
-            assert feature["activity_and_small_scatter"] == 0.0
-            assert feature["environment_and_small_scatter"] == 0.0
-            assert feature["label"] == "Regular"
+    result = generator.generate_report(info)
+    assert result is True
+    assert os.path.isfile("./data/balancing/balancing_report.json")
--- segregation_system/test/test_coverage_report_generator.py
+++ segregation_system/test/test_coverage_report_generator.py
@@ -1,25 +1,8 @@
+import os
 import sys
 import pytest
 sys.path.insert(0, r'../../segregation_system')
-from src.learning_sets_generator import LearningSetsGenerator
-
-
-@pytest.fixture
-def test_config():
-    return {
-    "stage": "store",
-    "segregation_system_ip": "0.0.0.0",
-    "segregation_system_port": "6000",
-    "development_system_ip": "10.8.0.2",
-    "development_system_port": "6000",
-    "preparation_system_ip": "10.8.0.6",
-    "preparation_system_port": "5000",
-    "db_name": "segregation.db",
-    "max_sessions": 400,
-    "train_set_size": 0.7,
-    "validation_set_size": 0.2,
-    "test_set_size": 0.1
-}
+from src.coverage_report_generator import CoverageReportGenerator
 
 @pytest.fixture
 def test_dataset():
@@ -176,26 +159,19 @@ def test_dataset():
         },
     ]
 
+def test_report(test_dataset):
+    generator = CoverageReportGenerator()
+    info = generator.generate_chart(test_dataset)
 
-def test_generate_learning_sets(test_config, test_dataset):
-    generator = LearningSetsGenerator(test_config)
-    result = generator.generate_learning_sets(test_dataset)
-    assert result["train"]["number_of_samples"] == 7
-    assert result["validation"]["number_of_samples"] == 2
-    assert result["test"]["number_of_samples"] == 1
+    assert os.path.isfile("./data/balancing/balancing_chart.png") == True
 
-    assert len(result["train"]["features"]) == 7
-    assert len(result["validation"]["features"]) == 2
-    assert len(result["test"]["features"]) == 1
+    assert info['maximum_pressure_ts'] == [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    assert info['minimum_pressure_ts'] == [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    assert info['median_pressure_ts'] == [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    assert info['mean_absolute_deviation_pressure_ts'] == [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    assert info['activity_and_small_scatter'] == [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    assert info['environment_and_small_scatter'] == [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 
-    categories = ["train" , "validation" , "test"]
-
-    for category in categories:
-        for feature in result[category]["features"]:
-            assert feature["maximum_pressure_ts"] == 0.0
-            assert feature["minimum_pressure_ts"] == 0.0
-            assert feature["median_pressure_ts"] == 0.0
-            assert feature["mean_absolute_deviation_pressure_ts"] == 0.0
-            assert feature["activity_and_small_scatter"] == 0.0
-            assert feature["environment_and_small_scatter"] == 0.0
-            assert feature["label"] == "Regular"
+    result = generator.generate_report(info)
+    assert result is True
+    assert os.path.isfile("./data/coverage/coverage_report.json")
